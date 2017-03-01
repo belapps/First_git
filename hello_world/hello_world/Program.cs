eba1a771f93d94c89a22663ae599696d44e3aac3
Bo Larsen - TEC <bola@TEC.dk>
2017-03-01 14:04:17 +0100

    added a 2 to line

--- hello_world/hello_world/Program.cs
+++ hello_world/hello_world/Program.cs
@@ -11,7 +11,7 @@ namespace hello_world
         static void Main(string[] args)
         {
             Console.WriteLine("Hello world");
-            Console.WriteLine("Hello world");
+            Console.WriteLine("Hello world2");
 
             Console.ReadKey();
         }
